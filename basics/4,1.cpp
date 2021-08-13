b2d04d4497c2203eff9f4fc56dc1893815ba4448
Jan Koželuh <46165619+Jankozeluh@users.noreply.github.com>
2021-08-13 12:09:18 +0200

    Create 4,1.cpp
    Je to zgarb.

---
+++ basics/4,1.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <unistd.h>
+//using namespace std;
+
+
+//Factory Method
+class Hero{
+    public:
+        Hero(std::string name, int baseHealth, int baseArmour, std::string activeAbility, int cooldown, std::string passiveAbility, int speed)
+        {
+            this->name=name;
+            this->baseHealth=baseHealth;
+            this->baseArmour=baseArmour;
+            this->activeAbility=activeAbility;
+            this->cooldown=cooldown;
+            this->passiveAbility=passiveAbility;
+            this->speed=speed;
+        }
+
+    private:
+        std::string name;
+        int baseHealth;
+        int baseArmour;
+        std::string activeAbility;
+        int cooldown;
+        std::string passiveAbility;
+        int speed;
+};
+
+class HeroFactory{
+    public:
+        Hero* generateRanger(){
+            return new Hero("Ranger – Slipgate Marine", 100, 25, "Dire Orb", 20, "Son of a Gun", 320);
+        }
+};
+//
+
+//Factory
+class Pistol{
+    public:
+        Pistol(std::string type, int rpm, int muzzleVelocity){
+            this->type=type;
+            this->rpm=rpm;
+            this->muzzleVelocity=muzzleVelocity;
+        }
+
+        static Pistol* glock(){
+            return new Pistol("semi-automatic",1200,375);
+        }
+
+    private:
+        std::string type;
+        int rpm;
+        int muzzleVelocity;
+};
+//
+
+//Singleton
+class Confusion{
+    public:
+        Confusion(const Confusion&) = delete; // zabraňuje předání instance hodnotou
+
+        static Confusion& Get(){
+            static Confusion instance;
+            return instance;
+        }
+        static std::string Nervy(){return Get().INervy();};
+    private:
+        std::string INervy(){ return m_VKyblu;};
+        Confusion(){}
+        std::string m_VKyblu = "Nervz";
+};
+//
+
+int main() {
+    HeroFactory* factory = new HeroFactory();
+    Hero* Ranger = factory->generateRanger();
+
+    Pistol::glock();
+
+    std::cout << Confusion::Nervy() << std::endl;
+
+    return 0;
+} 
