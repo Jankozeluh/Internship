d3517d5b82466b34e698b79255a6c9fbf1e42651
Jan Koželuh <46165619+Jankozeluh@users.noreply.github.com>
2021-08-24 11:49:39 +0200

    autobus

--- basics/oop/creation.php
+++ basics/oop/creation.php
@@ -1,56 +1,91 @@
 <?php
-require_once "models.php";
+require_once 'db.php';
+class Person {
+    private $firstName,$lastName,$birth;
+    //private array $birth = array();
 
-class StudentsFactory{
-    public static function createBjergsen():Student{return new Student('Soren','Bjerg', array(1996,2,21),array(2010,6,2),20);}
-    public static function createFaker():Student{return new Student('Lee','Sang-hjok', array(1996,5,7),array(2017,4,8),102/*6*/);}
-    public static function createMithy():Student{return new Student('Alfonso','Rodriguez', array(1994,10,5),array(2016,3,22),55);}
-    public static function createDoublelift():Student{return new Student('Yiliang','Peng', array(1993,7,19),array(2019,10,9),28);}
-    public static function createFreeze():Student{return new Student('Ales','Knezinek', array(1994,6,6),array(2020,10,11),27);}
+    public function __construct($firstName,$lastName,$birth){
+        $this->firstName=$firstName;
+        $this->lastName=$lastName;
+        $this->birth=$birth;
+//        $this->birth["day"] = $birth[2];
+//        $this->birth["month"] = $birth[1];
+//        $this->birth["year"] = $birth[0];
+    }
+
+    public function getFirstName(){ return $this->firstName;}
+    public function getLastName(){ return $this->lastName;}
+    public function getBirth(): string{ return $this->birth;}
+    //public function getBirth(): string{ return ($this->birth["year"]."/".$this->birth["month"]."/".$this->birth["day"]);}
+    /*public function setFirstName($firstName): void{$this->firstName = $firstName;}
+    public function setLastName($lastName): void{ $this->lastName = $lastName;}
+    public function setBirth($year,$month,$day): void{
+        $this->birth["day"] = $day;
+        $this->birth["month"] = $month;
+        $this->birth["year"] = $year;
+    }
+    */
 }
 
-class SubjectsFactory{
-    public static function createMath(): Subject{return new Subject('Math',4, 2,1,1);}
-    public static function createHistory(): Subject{return new Subject('History',6, 1,10,2);}
-    public static function createGeography(): Subject{return new Subject('Geography',7, 2,7,1);}
-    public static function createArt(): Subject{return new Subject('Art',5, 2,3,1);}
+class Student extends Person{
+    private int $credits;
+    private string $enrollDate;
+    //private array $enrollDate = array();
+
+    public function __construct($firstName, $lastName, $birth, $enrollDate, $credits) {
+        parent::__construct($firstName,$lastName,$birth);
+//        $this->enrollDate["day"] = $enrollDate[2];
+//        $this->enrollDate["month"] = $enrollDate[1];
+//        $this->enrollDate["year"] = $enrollDate[0];
+        $this->enrollDate=$enrollDate;
+        $this->credits=$credits;
+    }
+
+    //public function getEnrollDate(): string{ return $this->enrollDate["year"]."/".$this->enrollDate["month"]."/".$this->enrollDate["day"];}
+    public function getCredits(): int{ return $this->credits;}
+    public function getEnrollDate(): string{ return $this->enrollDate;}
+
+    /*public function setEnrollDate($year,$day,$month): void{
+        $this->enrollDate["day"] = $day;
+        $this->enrollDate["month"] = $month;
+        $this->enrollDate["year"] = $year;
+    }
+    public function setCredits($credits): void{ $this->credits = $credits;}
+    */
 }
 
-class TeachersFactory{
-    public static function createHolly(): Teacher{return new Teacher('MFA.','Holly', 'Holm',array(1976,2,5));}
-    public static function createKarel(): Teacher{return new Teacher('PhD.','Karel', 'Vavra',array(1956,7,8));}
-    public static function createPaul(): Teacher{return new Teacher('MD.','Paul', 'Horak',array(1986,12,6));}
-    public static function createAlbert(): Teacher{return new Teacher('MS.','Albert', 'Klokocka',array(1944,10,13));}
-    public static function createBilly(): Teacher{return new Teacher('BAS.','Billy', 'Farmer',array(1982,1,30));}
+class Teacher extends Person{
+    private string $degree;
+
+    public function __construct($degree,$firstName, $lastName, $birth) {
+        parent::__construct($firstName,$lastName,$birth);
+        $this->degree=$degree;
+    }
+
+    public function getDegree(): string{ return $this->degree;}
+    //public function setDegree($degree): void{ $this->degree = $degree;}
 }
 
-//$StudentsFactory = new StudentsFactory();
-//$TeachersFactory = new TeachersFactory();
-//$SubjectsFactory = new SubjectsFactory();
-//
-//$Bjergsen = $StudentsFactory::createBjergsen();
-//$Faker = $StudentsFactory::createFaker();
-//$Mithy = $StudentsFactory::createMithy();
-//$Doublelift = $StudentsFactory::createDoublelift();
-//$Freeze = $StudentsFactory::createFreeze();
-//$Holly = $TeachersFactory::createHolly();
-//$Albert = $TeachersFactory::createAlbert();
-//$Billy = $TeachersFactory::createBilly();
-//$Karel = $TeachersFactory::createKarel();
-//$Paul = $TeachersFactory::createPaul();
-//
-//$Math = $SubjectsFactory::createMath();
-//$History = $SubjectsFactory::createHistory();
-//$Geography = $SubjectsFactory::createGeography();
-//$Art = $SubjectsFactory::createArt();
+class Subject{
+    private $credits,$semester,$garant,$name,$pc;
 
-$Lukas = new Teacher('BA.','Lukas','Topol','2021/08/19');
+    public function __construct($name,$credits,$semester,$garant,$pc){
+        $this->name=$name;
+        $this->credits=$credits;
+        $this->semester=$semester;
+        $this->garant=$garant;
+        $this->pc=$pc;
+    }
 
-echo '<div class="container px-4 py-3"><div class="row"><div class="col-sm">';
-    Action::getStudents();
-echo '</div><div class="col-sm">';
-    Action::getTeachers();
-echo '</div><div class="col-sm">';
-    Action::getSubjects();
-echo '</div></div></div>';
-//Action::insertSubjectToPerson($Lukas,'Art',35,5);
+    public function getName(){ return $this->name;}
+    public function getCredits(){ return $this->credits;}
+    public function getSemester(){ return $this->semester;}
+    public function getGarant(){ return $this->garant;}
+    public function getPc(){ return $this->pc;}
+
+    /*public function setName($name): void{ $this->name = $name;}
+    public function setCredits($credits): void{ $this->credits = $credits;}
+    public function setSemester($semester): void{ $this->semester = $semester;}
+    public function setGarant($garant): void{ $this->garant = $garant;}
+    */
+}
